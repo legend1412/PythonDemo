617d23eaab918d32cba031bc3cf0c01cbb191544
legend1412 <zhaojianhao_1984@163.com>
2020-05-28 00:00:28 +0800

    FM

--- dt/example/fm.py
+++ dt/example/fm.py
@@ -6,17 +6,14 @@ from sklearn.model_selection import train_test_split
 
 idir = 'E://GitHub//PythonDemo//data//'
 feature_length = 18
+tf.compat.v1.disable_eager_execution()
 
 
 class FM(object):
-    """
-    Factorization Machine with FTRL optimization
-    """
+    """Factorization Machine with FTRL optimization"""
 
     def __init__(self, config):
-        """
-        :param config:configuration of hyperparameters type of dict
-        """
+        """config:configuration of hyperparameters type of dict"""
         # number of latentfactors
         self.k = config['k']
         self.lr = config['lr']
@@ -27,56 +24,58 @@ class FM(object):
         self.p = feature_length
 
     def add_placholders(self):
-        self.X = tf.sparse_placeholder('float32', [None, self.p])
-        self.y = tf.paceholder('int64', [None, ])
-        self.keep_prob = tf.placeholder('float32')
+        """add_placholders"""
+        self.X = tf.compat.v1.sparse_placeholder('float32', [None, self.p])
+        self.y = tf.compat.v1.placeholder('int64', [None, ])
+        self.keep_prob = tf.compat.v1.placeholder('float32')
 
     def inference(self):
         """
         forward propagation
         :return: labels for each sample
         """
-        with tf.variable_scope('linear_layer'):
-            b = tf.get_variable('bias', shape=[2], initializer=tf.zeros_initializer())
-            w1 = tf.get_variable('w1', shaple=[self.p, 2],
-                                 initializer=tf.rruncated_normal_initializer(mean=0, stddev=1e-2))
+        with tf.compat.v1.variable_scope('linear_layer'):
+            b = tf.compat.v1.get_variable('bias', shape=[2], initializer=tf.zeros_initializer())
+            w1 = tf.compat.v1.get_variable('w1', shape=[self.p, 2],
+                                           initializer=tf.compat.v1.truncated_normal_initializer(mean=0, stddev=1e-2))
 
             # shape of [None,2]
-            self.linear_terms = tf.add(tf.sparse_tensor_dense_matmul(self.x, w1), b)
+            self.linear_terms = tf.add(tf.compat.v1.sparse_tensor_dense_matmul(self.X, w1), b)
 
-        with tf.variable_scope('interaction_layer'):
-            v = tf.get_variable('v', shape=[self.p, self.k],
-                                initializer=tf.truncated_normal_initializer(mean=0, seddev=0.01))
+        with tf.compat.v1.variable_scope('interaction_layer'):
+            v = tf.compat.v1.get_variable('v', shape=[self.p, self.k],
+                                          initializer=tf.compat.v1.truncated_normal_initializer(mean=0, stddev=0.01))
             # shape of [None,1]
             self.interaction_terms = tf.multiply(0.5, tf.reduce_mean(
-                tf.subtract(tf.show(tf.sparse_tensor_dense_matmul(self.X, v), 2),
-                            tf.sparse_tensor_dense_matmul(self.x, tf.pow(v, 2))),
-                1, keep_dims=True))
+                tf.subtract(tf.pow(tf.compat.v1.sparse_tensor_dense_matmul(self.X, v), 2),
+                            tf.compat.v1.sparse_tensor_dense_matmul(self.X, tf.pow(v, 2))),
+                1, keepdims=True))
 
         # shape of [None,2]
         self.y_out = tf.add(self.linear_terms, self.interaction_terms)
         self.y_out_prob = tf.nn.softmax(self.y_out)
 
     def add_loss(self):
+        """addloss"""
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y, logits=self.y_out)
         mean_loss = tf.reduce_mean(cross_entropy)
         self.loss = mean_loss
         tf.summary.scalar('loss', self.loss)
 
     def add_accuracy(self):
-        # accuracy
+        """accuracy"""
         self.correct_prediction = tf.equal(tf.cast(tf.argmax(model.y_out, 1), tf.int64), model.y)
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
         # add summary to accuracy
         tf.summary.scalar('accuracy', self.accuracy)
 
     def train(self):
-        # Applies exponential decay to learning rate
+        """Applies exponential decay to learning rate"""
         self.global_step = tf.Variable(0, trainable=False)
         # define outimizer
-        optimizer = tf.train.FtrlOptimizer(self.lr, l1_regularization_strength=self.reg_l1,
-                                           l2_regularization_strength=self.reg_l2)
-        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        optimizer = tf.compat.v1.train.FtrlOptimizer(self.lr, l1_regularization_strength=self.reg_l1,
+                                                     l2_regularization_strength=self.reg_l2)
+        extra_update_ops = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(extra_update_ops):
             self.train_op = optimizer.minimize(self.loss, global_step=self.global_step)
 
@@ -101,16 +100,22 @@ def check_restore_parameters(sess, saver):
 
 # 有放回的采样，以采样一个batch_size大小的数据
 def get_random_block_from_data(data, labels, batch_size):
+    """
+    :param data:
+    :param labels:
+    :param batch_size:
+    :return:
+    """
     start_index = np.random.randint(0, data.shape(0) - batch_size)  # bathc_size =2,len(data)=10 [0,9]
     return data.iloc[start_index:(start_index + batch_size), :], labels[start_index:(start_index + batch_size)]
 
 
 if __name__ == '__main__':
     df_train = pd.read_csv(idir + 'train_feat.csv').fillna(0.)
-    labels = np.load(idir + 'labels.npy')
-    X_train, X_test, y_train, y_test = train_test_split(df_train, labels, test_size=0.2, random_state=2019)
+    init_labels = np.load(idir + 'labels.npy')
+    X_train, X_test, y_train, y_test = train_test_split(df_train, init_labels, test_size=0.2, random_state=2019)
     del df_train
-    del labels
+    del init_labels
     f_to_use = ['user_total_orders', 'user_total_items', 'total_distinct_items',
                 'user_average_days_between_orders', 'user_average_basket',
                 'order_hour_of_day', 'days_since_prior_order', 'days_since_ratio',
@@ -119,12 +124,7 @@ if __name__ == '__main__':
                 'UP_average_pos_in_cart', 'UP_orders_since_last',
                 'UP_delta_hour_vs_last']
 
-    config = {}
-    config['lr'] = 0.01
-    config['batch_size'] = 512
-    config['reg_l1'] = 2e-2
-    config['reg_l2'] = 0
-    config['k'] = 40
+    config = {'lr': 0.01, 'batch_size': 512, 'reg_l1': 2e-2, 'reg_l2': 0, 'k': 40}
 
     # print(X_train.head())
     # print(X_train[f_to_use])
@@ -135,14 +135,14 @@ if __name__ == '__main__':
     # build graph for model
     model.build_graph()
 
-    saver = tf.train.Saver(max_to_keep=5)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+    init_saver = tf.compat.v1.train.Saver(max_to_keep=15)
+    with tf.compat.v1.Session() as init_sess:
+        init_sess.run(tf.compat.v1.global_variables_initializer())
         # restore trained parameters
-        check_restore_parameters(sess, saver)
+        check_restore_parameters(init_sess, init_saver)
         # train
-        merged = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter('train_logs', sess.graph)
+        merged = tf.compat.v1.summary.merge_all()
+        train_writer = tf.compat.v1.summary.FileWriter('train_logs', init_sess.graph)
         epochs = 30
 
         for e in range(epochs):
@@ -151,7 +151,7 @@ if __name__ == '__main__':
             for ibatch in range(num_batches):
                 batch_x, batch_y = get_random_block_from_data(X_train, y_train, config['batch_size'])
                 feed_dict = {model.X: batch_x.to_sparse, model.y: batch_y, model.keep_prob: 0.5}
-                batch_loss, acc, summary, global_step, _ = sess.run(
+                batch_loss, acc, summary, global_step, _ = init_sess.run(
                     [model.loss, model.accuracy, merged, model.global_step, model.train_op], feed_dict=feed_dict)
 
                 print(batch_loss)
