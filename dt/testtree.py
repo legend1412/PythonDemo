ef64d8f8c02d19eb4ad76227fa73c4c8f76d2c55
legend1412 <zhaojianhao_1984@163.com>
2020-05-29 10:55:55 +0800

    tree

--- dt/testtree.py
+++ dt/testtree.py
@@ -12,7 +12,7 @@ X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test
 
 clf = DecisionTreeClassifier(criterion="gini", splitter="best", max_depth=None, min_samples_split=2,
                              min_samples_leaf=1, min_weight_fraction_leaf=0., max_features=None,
-                             random_state=None, max_leaf_nodes=None, class_weight=None, presort=None)
+                             random_state=None, max_leaf_nodes=None, class_weight=None)
 
 dt_model = clf.fit(X_train, y_train)
 print(cross_val_score(clf, iris.data, iris.target, cv=10))
@@ -20,11 +20,12 @@ print(cross_val_score(clf, iris.data, iris.target, cv=10))
 dot_data = tree.export_graphviz(dt_model, out_file='tree.dot', feature_names=iris.feature_names,
                                 class_names=iris.target_names, filled=True, rounded=True,
                                 special_characters=True)
+
 print(dot_data)
 
-os.environ["PATH"] += os.pathsep + 'D:\\'
-graph = pydotplus.graph_from_dot_data(dot_data)
-# graph = pydotplus.graph_from_dot_file('tree.dot')
-Image(graph.create_png())
+os.environ["PATH"] += os.pathsep + 'D:\\soft\\Graphviz2.38\\bin'
+# graph = pydotplus.graph_from_dot_data(dot_data)
+graph = pydotplus.graph_from_dot_file('tree.dot')
+# Image(graph.create_png())
 # graph.write_pdf("wine.pdf")
-
+graph.write_png("wine.png")
