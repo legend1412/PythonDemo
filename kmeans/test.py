c9e390142b50fb8b6e69ae53e5d034a0f30ee133
legend1412 <zhaojianhao_1984@163.com>
2020-06-11 14:00:23 +0800

    TEST

--- kmeans/test.py
+++ kmeans/test.py
@@ -1,4 +1,40 @@
+import os
+import math
+import operator
+import random
+import re
+from collections import Counter
+from kmeans.k_means import load_data
 
+print(os.getcwd())
+print(math.cos(20))
+print(operator.abs(1))
+
+file_path = '../data/allfiles'
+
+K = 3
+doc_dict, doc_list = load_data()
+doc1_dict = doc_dict['734business']
+doc2_dict = doc_dict['1643auto']
+words = set(doc1_dict).union(set(doc2_dict))
+print(len(words))
+print(words)
+k_doc_list = random.sample(doc_list, K)
+print(doc_list)
+print(k_doc_list)
+print(random.random() * 27)
+
+label_pattern = re.compile(r'[a-z]')
+print(label_pattern.findall('12business'))
+print('------------------------------------------------------------')
+a = dict()
+for i in range(3):
+    a[i] = '%s' % i
+print([i for i in a.keys()])
+print(Counter([i for i in a.keys()]))
+print(abs(-1))
+
+print('------------------------------------------------------------')
 word_dict = dict()
 word_freq = dict()
 line = "business yule business it sports auto"
