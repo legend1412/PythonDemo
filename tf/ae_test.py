566137426dc865349be594521b1cd377568a79f9
legend1412 <zhaojianhao_1984@163.com>
2020-06-16 00:30:58 +0800

    tf

--- tf/ae_test.py
+++ tf/ae_test.py
@@ -1,8 +1,15 @@
 # -*- coding:UTF-8 -*-
 
-import tensorflow as tf
+import tensorflow_core as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow_core.examples.tutorials.mnist import input_data
 from PIL import Image
-tf.compat.v1.reset_default_graph()
+
+tf.reset_default_graph()
+
+# # 读入数据
+mnist = input_data.read_data_sets('../data/MNIST/', one_hot=True)
+print(mnist.train.images.shape, mnist.train.labels.shape)  # (55000,784)(55000,10)
+print(mnist.test.images.shape, mnist.test.labels.shape)  # (10000,784)(1000,10)
+print(mnist.validation.images.shape, mnist.validation.labels.shape)  # (5000,784)(5000,10)
