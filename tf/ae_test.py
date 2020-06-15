47c7e449717c84e280ba31c8854bc16a0e656d91
legend1412 <zhaojianhao_1984@163.com>
2020-06-15 16:58:15 +0800

    tf

---
+++ tf/ae_test.py
@@ -0,0 +1,8 @@
+# -*- coding:UTF-8 -*-
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+from PIL import Image
+tf.compat.v1.reset_default_graph()
