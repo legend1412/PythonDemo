80a9aea6dbbd1f0b6b6c29d0bb7f691c8c752d64
legend1412 <zhaojianhao_1984@163.com>
2020-06-23 00:46:43 +0800

    cnn

--- tf/cnn.py
+++ tf/cnn.py
@@ -4,8 +4,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('../data/MNIST/', one_hot=True)
 
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
-sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.6)
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
 
 
 # sess = tf.Session()
@@ -13,7 +13,7 @@ sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 # 权重初始化
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.random.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 
@@ -29,7 +29,7 @@ def conv2d(x1, w):
 
 
 def max_pool_2x2(x1):
-    return tf.nn.max_pool(x1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool2d(x1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 # 卷积核大小5*5 -> 32 feature map,1表示channel，黑白
@@ -37,7 +37,7 @@ w_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
 # 取到的是one_hot形式的图片，即在一个数组中
-x = tf.placeholder(tf.float32, shape=[None, 784])
+x = tf.compat.v1.placeholder(tf.float32, shape=[None, 784])
 # reshape将数组转化成28*28矩阵大小，即图片
 x_image = tf.reshape(x, [-1, 28, 28, 1])
 
@@ -67,7 +67,7 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
 # dropout
 # 1、按照一定概率丢失节点，可以减少过拟合
 # 2、自动处理输出值的scale
-keep_prob = tf.placeholder(tf.float32)
+keep_prob = tf.compat.v1.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob=keep_prob)
 
 # 输出层
@@ -76,25 +76,30 @@ b_fc2 = bias_variable([10])
 
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
 # 训练设置
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
+y_ = tf.compat.v1.placeholder(tf.float32, shape=[None, 10])
+cross_entropy = -tf.reduce_sum(y_ * tf.math.log(y_conv))
 
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.compat.v1.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 # 预测设置 index[0,1,2,3,4,5,6,7,8,9] [0.1,0.01,0.03,0.05,0.5,0.2,0.01,...]  [0,0,0,0,1,0,0,0,0,...]\
 correct_pred = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))  # [0,0,1,1,1]
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 # sess.run(tf.initialize_all_variables())
-sess.run(tf.global_variables_initializer())
+sess.run(tf.compat.v1.global_variables_initializer())
 
 # 开始训练打印日志
-for i in range(20000):
+total = 0
+good = 0
+for i in range(2000):
     batch = mnist.train.next_batch(50)
+    total += batch[0].shape[0]
     if i % 1000 == 0:
         # 预测过程需要所有节点都起到作用，所以keep_prob:1.0
         train_accuracy = accuracy.eval(session=sess, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        good += train_accuracy
         print('step:%d,training accuracy %g' % (i, train_accuracy))
     # 训练过程需要将部分节点dropout，所以要给定一个概率0.5 early_stop
     train_step.run(session=sess, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-    print('Test accuracy %g' % accuracy.eval(session=sess,
-                                             feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    # print('Test accuracy %g' % accuracy.eval(session=sess,
+    #                                          feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+print('Good: %g,Total: %g,Test accuracy:%g' % (good, total, (good / total)))
